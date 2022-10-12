064bdc741e3a65bb6c5a2c622b50eb7f4c0281e1
AlanBo <70921209+alabort@users.noreply.github.com>
2022-10-11 23:36:03 -0400

    Delete prova 1

--- README.md
+++
@@ -1,1 +0,0 @@
-This is the blog of Alan Bortolotti and has been realized with tailwind and VUE.
--- data/menu.js
+++
@@ -1,6 +0,0 @@
-const menu = [
-  { name: "Home", path: "/" },
-  { name: "Projects", path: "/projects" },
-];
-
-export default menu;
